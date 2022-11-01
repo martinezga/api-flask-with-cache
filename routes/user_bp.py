4dcc76f6571578a9d4155e6e0ca07cb6f498a7bd
Gabriella Martinez <52822208+martinezga@users.noreply.github.com>
2022-11-01 19:05:13 -0300

    Added user blueprint slashes

--- routes/user_bp.py
+++ routes/user_bp.py
@@ -4,5 +4,5 @@ from controllers import user_controller
 
 user_bp = Blueprint('user_bp', __name__, url_prefix="/auth/users")
 
-user_bp.route('', methods=['GET'])(user_controller.list_all)
-user_bp.route('', methods=['POST'])(user_controller.create)
+user_bp.route('/', methods=['GET'])(user_controller.list_all)
+user_bp.route('/', methods=['POST'])(user_controller.create)
